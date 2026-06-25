06b0ed81dced2dcb8bca23eb1de0c7f7633e07d9
Ahmed A. Aswad <65052566+ahmedabdghaney@users.noreply.github.com>
2026-06-26 00:18:37 +0300

    fdfd

--- src/components/videos.ts
+++ src/components/videos.ts
@@ -1,3 +1,3 @@
 export const videoLinks: Record<string, string> = {
-  "movie_1315772": "https://d269k7J205s3hx.cloudfront.net/IMG_1944.mp4"
+  "movie_1315772": "https://playmogo.com/e/37kgjmki72g8"
 };
